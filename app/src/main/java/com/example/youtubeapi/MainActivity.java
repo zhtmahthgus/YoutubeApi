dc224df7544829d92e003319571dc4d19778135e
zhtmahthgus <zhtmahthgus@naver.com>
2021-02-25 14:15:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/youtubeapi/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.youtubeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView youtubeView;
+    Button button;
+    YouTubePlayer.OnInitializedListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.youtubeButton);
+        youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+        listener = new YouTubePlayer.OnInitializedListener(){
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("vewH-f3fAes");//유튜브 링크
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youtubeView.initialize("ApiKey",listener);//API 키
+            }
+        });
+    }
+}
